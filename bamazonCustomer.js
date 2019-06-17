105768af58dc0e90c1fbf1dc4020258647d44917
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-06-16 22:38:12 -0700

    almost there. Cannot figure out why i cannot combine the res and the id but they both exist.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,32 +17,82 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  start();
+
+  createTable();
+
 });
 
-function start() {
+function createTable() {
+  
+  var query = "SELECT * FROM products";
+  connection.query(query, function(err, res) {
+    if (err) throw err;
 
-    var query = "SELECT * FROM products";
-    connection.query(query, function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-    });
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item ID: " + res[i].item_id + "\nProduct Name: " + res[i].product_name + "\nDepartment Name: " + res[i].department_name + "\nPrice: " + res[i].price + "\nStock Quantity: " + res[i].stock_quantity);
+      console.log("-------------------------------------------------------");
+    }
+
+    start(res);
+  });
+}
+
+function start(res) {
+
+  inquirer
+  .prompt(
+  {
+    name: "id",
+    type: "input",
+    message: "What is the ID of the product you would like to buy?",
+    validate: function (value) {
+      if (isNaN (value) == false) {
+          return true;
+      } else {
+          return false;
+      }
+    }
+  })
+  .then(function(answer) {
+
+    if (answer.id > res.length) {
+      console.log("Thats not a valid ID");
+      start(res);
+    }
+
+    var id = answer.id;
 
     inquirer
-    .prompt([
+    .prompt(
     {
-      name: "id",
+      name: "units",
       type: "input",
-      message: "What is the ID of the product you would like to buy?"
-    },
-    {
-        name: "units",
-        type: "input",
-        message: "How many units of this product?"
-    }])
+      message: "How many units of this product?",
+      validate: function (value) {
+        if (isNaN (value) == false) {
+            return true;
+        } else {
+            return false;
+        }
+      }
+    })
     .then(function(answer) {
 
+    console.log(res);
+    
+    if (answer.units > res[id].stock_quantity) {
+      console.log("Store does have that much in stock");
+      start(res);
+    }
 
+    var query = "UPDATE products SET stock_quantity='" + (res[id].stock_quantity - answer.units) + "' WHERE item_id='" + id +"'";
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+
+      var cost = (answer.units * res[id].price)
+
+      console.log("You bought " + answer.units + " " + res[id].item_id + " for " + cost);
     });
+  })
+  });
 }
